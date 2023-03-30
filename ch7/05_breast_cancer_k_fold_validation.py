62419c6cae9fc5c5f5f66a21eaef2fe57dac1ac9
Edgar Saavedra <edgarsaavedraa@gmail.com>
2023-03-30 16:45:20 -0500

    custom k-fold validation

--- ch7/05_breast_cancer_k_fold_validation.py
+++ ch7/05_breast_cancer_k_fold_validation.py
@@ -21,7 +21,8 @@ def run(train_set, train_labels, test_set, test_labels, model):
   model.fit(train_set, train_labels)
   return model.score(test_set, test_labels)
 
-
+# take data set samples, the corresponding labels, the current fold number , and total number of folds
+# deivid the full dataset in to m distinct number of folds
 def split(dataset_samples, dataset_labels, current_k_fold_number, total_number_folds):
   # divide the full dataset in to number of folds
   number_samples_per_fold = int(dataset_labels.shape[0]/total_number_folds)
@@ -33,13 +34,14 @@ def split(dataset_samples, dataset_labels, current_k_fold_number, total_number_f
   for i in range(total_number_folds):
     # each element of this list contains the feature vectors and labels of the fold
     list_of_folds.append(
-      dataset_samples[(number_samples_per_fold * i):(number_samples_per_fold * i + number_samples_per_fold)],
-      dataset_labels[(number_samples_per_fold * i):(number_samples_per_fold * i + number_samples_per_fold)]
+      [
+        dataset_samples[(number_samples_per_fold * i):(number_samples_per_fold * i + number_samples_per_fold)],
+        dataset_labels[(number_samples_per_fold * i):(number_samples_per_fold * i + number_samples_per_fold)]
+      ]
     )
 
   # test set is the k-th fold so we set those values
-  test_vectors = list_of_folds[current_k_fold_number]
-  test_labels = list_of_folds[current_k_fold_number]
+  test_vectors, test_labels = list_of_folds[current_k_fold_number]
 
 
   train_vectors = []
@@ -51,8 +53,8 @@ def split(dataset_samples, dataset_labels, current_k_fold_number, total_number_f
     if (i == current_k_fold_number):
       continue
     else:
-      a = list_of_folds[i]
-      b = list_of_folds[i]
+      a,b = list_of_folds[i]
+      list_of_folds[i]
       train_vectors.append(a)
       train_labels.append(b)
   
@@ -63,13 +65,9 @@ def split(dataset_samples, dataset_labels, current_k_fold_number, total_number_f
   # - reshape it so that training vectors has 30 columns (the number of features per vector) and number of samples per fold
   # - train vectors then becomes train_vectors minus the samples we put into the test fold
   # - we buld a train label so that the correct label goes with each feature in the train vectors
-  train_vectors = np.array(train_vectors).reshape(
-      ((total_number_folds - 1) * number_samples_per_fold, 30)
-    )
+  train_vectors = np.array(train_vectors).reshape(((total_number_folds-1)*number_samples_per_fold,30))
 
-  train_labels = np.array(train_labels).reshape(
-      ((total_number_folds - 1) * number_samples_per_fold)
-    )
+  train_labels = np.array(train_labels).reshape((total_number_folds-1)*number_samples_per_fold)
 
   return [
     train_vectors,
@@ -81,18 +79,48 @@ def split(dataset_samples, dataset_labels, current_k_fold_number, total_number_f
 # pretty print function to show the per split scores
 # it also shows the average score acorss all the splits
 # The score is the overall accuracy of the model, 1 is perfection 0 is failure
-def prettyPrint(z, k_fold, per_split_core):
-  m = z.shape[1]
-  print("%-19s: %0.4f +/- %0.4f | " % (
-      per_split_core, 
-      z[k_fold].mean(), # average
-      z[k_fold].std()/np.sqrt(m)
-    ),
-    end=''
-  )
-
+def prettyPrint(vector, k_fold, per_split_score):
+  m = vector.shape[1]
+  print("%-19s: %0.4f +/- %0.4f | " % (per_split_score, vector[k_fold].mean(), vector[k_fold].std()/np.sqrt(m)), end='')
   for i in range(m):
-    print("%0.4f " % z[k_fold, i], end='')
-
+      print("%0.4f " % vector[k_fold,i], end='')
   print()
 
+
+def main():
+  # load full dataset
+  x = np.load("../ch5/breast_cancer_features.npy")
+  y = np.load("../ch5/breast_cancer_labels.npy")
+
+  # randomize ordering
+  idx = np.argsort(np.random.random(y.shape[0]))
+  
+  x = x[idx]
+  y = y[idx]
+
+  # read number of folds
+  m = int(sys.argv[1])
+  # this array holds th per fold score for each of the eight models
+  z = np.zeros((8,m))
+
+  for k in range(m):
+    x_train, y_train, x_test, y_test = split(x,y,k,m)
+    z[0,k] = run(x_train, y_train, x_test, y_test, NearestCentroid())
+    z[1,k] = run(x_train, y_train, x_test, y_test, KNeighborsClassifier(n_neighbors=3))
+    z[2,k] = run(x_train, y_train, x_test, y_test, KNeighborsClassifier(n_neighbors=7))
+    z[3,k] = run(x_train, y_train, x_test, y_test, GaussianNB())
+    z[4,k] = run(x_train, y_train, x_test, y_test, DecisionTreeClassifier())
+    z[5,k] = run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=5))
+    z[6,k] = run(x_train, y_train, x_test, y_test, RandomForestClassifier(n_estimators=50))
+    z[7,k] = run(x_train, y_train, x_test, y_test, SVC(kernel="linear", C=1.0))
+
+  prettyPrint(z, 0, "Nearest")
+  prettyPrint(z, 1, "3-NN")
+  prettyPrint(z, 2, "7-NN")
+  prettyPrint(z, 3, "Naive Bayes")
+  prettyPrint(z, 4, "Decision Tree")
+  prettyPrint(z, 5, "Random Forest (5)")
+  prettyPrint(z, 6, "Random Forest (50)")
+  prettyPrint(z, 7, "SVM (linear)")
+
+main()
